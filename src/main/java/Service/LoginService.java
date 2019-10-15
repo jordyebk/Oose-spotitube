69785cfb2f2647bf75d1dc37b56070870f07fc24
Jordy Veldhuizen <J.Veldhuizen@student.han.nl>
2019-10-15 20:50:32 +0200

    20:50

--- src/main/java/Service/LoginService.java
+++ src/main/java/Service/LoginService.java
@@ -1,7 +1,21 @@
 package Service;
 
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
 
-@Path()
+@Path("/login")
 public class LoginService {
+    @POST
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response loginUser(){
+        return Response.status(401).build();
+    }
+
+    @GET
+    @Produces("application/json")
+    public Response test(){
+
+        return Response.status(401).build();
+    }
 }
